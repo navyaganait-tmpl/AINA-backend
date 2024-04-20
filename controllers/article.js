bf56d75681a53279c6e6e2ac9e3e156b42039034
tanvayvaidya-tmpl <tanvay.v@tunica.tech>
2024-04-20 14:14:12 +0530

    extracting and saving data in database done

--- controllers/article.js
+++ controllers/article.js
@@ -6,7 +6,77 @@ const { JSDOM } = require('jsdom');
 
 module.exports = {
   getAllBlogs: async (req, res) => {
-    try {
+    try {const axios = require('axios');
+    const cheerio = require('cheerio');
+    const Parser = require('rss-parser');
+    const sources = require('../sources.json');
+    const Sequelize = require('sequelize');
+    const db = require('../database/models');
+    const parser = new Parser();
+    
+    async function scrapeArticleContent(url) {
+      try {
+        const response = await axios.get(url);
+        const $ = cheerio.load(response.data);
+    
+        // Remove unwanted tags
+        $('a, img, picture, source').remove(); // Remove all <a>, <img>, <picture>, and <source> tags
+    
+        // Get the cleaned article content
+        const articleContent = $('article').text().trim(); // You may need to adjust the selector based on the structure of the article
+    
+        return articleContent;
+      } catch (error) {
+        console.error(`Error scraping article content from ${url}: ${error.message}`);
+        return null;
+      }
+    }
+    
+    async function getAllBlogs(req, res) {
+      try {
+        let modifiedFeed = [];
+        for (const sourceUrl of sources) {
+          const feedUrl = sourceUrl.url;
+          const feed = await parser.parseURL(feedUrl);
+    
+          // Modify each feed item
+          const modifiedItems = await Promise.all(feed.items.map(async (item) => {
+            if (item.contentSnippet.split(' ').length < 50) {
+              const articleContent = await scrapeArticleContent(item.link);
+              if (articleContent) {
+                item.content = articleContent;
+                item['content:encoded'] = articleContent;
+                item.contentSnippet = articleContent.substring(0, 100) + '...'; // Update contentSnippet if needed
+              }
+            }
+            item.source = sourceUrl.source;
+            // console.log(db.);
+            await savingDataInDatabase(item['content:encoded'] , item.pubDate , item.source , item)
+            return item;
+          }));
+    
+          modifiedFeed = modifiedFeed.concat(modifiedItems);
+        }
+        res.send(modifiedFeed);
+      } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+      }
+    }
+    
+    
+    async function savingDataInDatabase(orginalDesc, pubDate, source, item) {
+      try {
+        console.log('-------------Saving Data in Database ------------------');
+    
+        await db.article.create({ orginalDesc: orginalDesc, publishedDate: pubDate, source: source, item: item });
+        console.log('-------------Saving Data in Database Completed------------------');
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    module.exports = { getAllBlogs };
+    
       const urls = [
         "https://feeds.feedburner.com/ndtvnews-top-stories",
         "https://www.deccanchronicle.com/google_feeds.xml",
--- database/models/article.js
+++ database/models/article.js
@@ -1,39 +1,48 @@
-const {DataTypes, BelongsTo}= require("sequelize");
-module.exports=(sequelize,DataTypes)=>{
-    const article =sequelize.define("article",{
-        articleId:{
-            primaryKey:true,
-            type:DataTypes.INTEGER,
-            auto_increment:true
+const { DataTypes, BelongsTo } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+    const article = sequelize.define("article", {
+        articleId: {
+            primaryKey: true,
+            autoIncrement: true,
+            type: DataTypes.INTEGER,
         },
-        orginalDesc:{
-            type:DataTypes.TEXT,
+        orginalDesc: {
+            type: DataTypes.TEXT,
             allowNull: false,
         },
-        generatedSummary:{
-            type:DataTypes.TEXT,
-            allowNull: false,
-        },
-        image:{
-            type:DataTypes.TEXT,
+        generatedSummary: {
+            type: DataTypes.TEXT,
             allowNull: true,
         },
-        originalUrl:{
-            type:DataTypes.TEXT,
+        image: {
+            type: DataTypes.TEXT,
             allowNull: true,
         },
-        publishedDate:{
-            type:DataTypes.DATE,
+        originalUrl: {
+            type: DataTypes.TEXT,
             allowNull: true,
+        },
+        publishedDate: {
+            type: DataTypes.DATE,
+            allowNull: true,
+        },
+
+        source: {
+            type: DataTypes.TEXT,
+            allowNull: true
+        },
+        originalArticle: {
+            type: DataTypes.JSON,
+            allowNull: true
         }
-        
-        
+
+
     })
     article.associate = function (models) {
         models.article.hasMany(models.article, { foreignKey: "articleId" });
         models.article.belongsTo(models.article, { foreignKey: "articleId" });
         models.article.belongsTo(models.category, { foreignKey: "categoryId" });
-      };
+    };
     return article;
 
 }
